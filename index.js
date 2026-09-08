356852984e97780fd8ca622c21ffc6c835e2f5cb
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-09-07 22:03:59 -0300

    agregar archivos principales de aplicacion

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(
+    "NodeJS Digitalers",
+    "\nPunto de Entrada Principal",
+    "\nEl codigo de la aplicacion va aqui",
+    "\nPresiona CTRL+C para terminar la ejecucion"
+);
---
+++ tests/index.js
@@ -0,0 +1,5 @@
+console.log(
+    "NodeJS Digitalers:",
+    "\n No se especificaron Pruebas",
+    "\n Intenta en otro momento"
+);
