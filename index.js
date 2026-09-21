67d66bd7e3709fc80398d6857986a0d97e0fb7d1
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-09-21 19:01:18 -0300

    ejemplo de aplicacion con express framework

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-console.log(
-    "NodeJS Digitalers",
-    "\nPunto de Entrada Principal",
-    "\nEl codigo de la aplicacion va aqui",
-    "\nPresiona CTRL+C para terminar la ejecucion"
-);
+import { configDotenv } from "dotenv";
+import express from "express";
+
+const dotEnv = configDotenv()
+const { parsed: {
+    HOST,
+    PORT,
+    URI,
+}} = dotEnv
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({
+        method: req.method,
+        message: "NO SE ENCONTRARON DATOS"
+    })
+});
+app.post("/", (req, res) => {
+    res.json({
+        method: req.method,
+        message: "SOLICITUD CARGADA EXITOSAMENTE"
+    })
+});
+app.put("/", (req, res) => {
+    res.json({
+        method: req.method,
+        message: "DATOS ACTUALIZADOS EXITOSAMENTE"
+    })
+});
+app.delete("/", (req, res) => {
+    res.json({
+        method: req.method,
+        message: "DATOS ELIMINADOS EXITOSAMENTE"
+    })
+});
+
+app.listen(PORT);
